b6bab4a0a3b1c8a50fcfbf6196d88cda1155049a
Felix <felix@stkn.org>
2012-10-25 00:36:05 +0200

    add u5

---
+++ u5/mainfile.c
@@ -0,0 +1,23 @@
+/*******************************************************************
+/ Programm    : Bruchrechnung                                         
+/ Verfasser   : Schmidt                                           
+/ Datum       : Urprogramm: 24.10.2012                                          
+/ Eingabe     : 2 rationale Zahlen                          
+/ Verarbeitung: diverse Berechnungen                   
+/ Änderungen  : 24.10.2012
+/ *******************************************************************/
+
+/* Einbinden von nötigen Header-Dateien                             */
+#include <stdio.h>    /* Standard Input/ Output  z.B. scanf, printf */
+#include <stdlib.h>   /* Standard-Bibliothek, z.B. für system       */
+#include "mainfile.h"
+
+
+int main()
+{
+	printf("Bitte Zaehler und Nenner Bruch 1 eingeben: ");
+
+	system("pause");
+	return(0);
+}
+
---
+++ u5/mainfile.h
@@ -0,0 +1,7 @@
+#ifndef _MAINFILE_H_
+#define _MAINFILE_H_
+
+/* Prototypen der benötigten Funktionen */
+
+
+#endif
