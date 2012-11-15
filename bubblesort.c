44585a784dc7b0c2f839a75efb0553ecb4f17d9c
Felix <felix@stkn.org>
2012-11-15 20:26:20 +0100

    dos2unix again ...

--- bubblesort.c
+++ bubblesort.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 void bubblesort(int *array, int length);
 
 int main()
 {
 	int feld[] = {1, 2, 10, 5, 3, 2, 8};
-	int anzEl = sizeof(feld);
-
-	bubblesort(&feld, anzEl);
-
+	int anzEl = sizeof(feld) / 4;
 	int i;
+
+	bubblesort(feld, anzEl);
 
 	for(i = 0; i < anzEl - 1; i++)
 		printf("%d, ", feld[i]);
@@ -22,15 +22,15 @@ int main()
 
 void bubblesort(int *array, int length)
 {
-	int i, j;
+	int i, j, tmp;
 
-	for (i = 0; i < length - 1; --i)
+	for (i = 0; i < length - 1; ++i)
 	{
-		for (j = 0, j < length - i - 1; ++j)
+		for (j = 0; j < length - i - 1; ++j)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int tmp = array[j];
+				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 			}
