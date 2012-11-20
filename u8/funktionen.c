3fbe296406ce302995b7f9c6293bc20c95f5ce5f
Felix <felix@stkn.org>
2012-11-20 12:58:03 +0100

    some fixes

--- u8/funktionen.c
+++ u8/funktionen.c
@@ -3,7 +3,7 @@
 // Verfasser   : Schmidt                                             
 // Datum       : Urprogramm: 13.11.2012                                           
 // Aufgabe     : Primzahlberechnung nach Sieb des Eratosthenes
-// Änderungen  : 13.11.2012
+// Ã„nderungen  : 13.11.2012
 *******************************************************************/
 
 #include <stdio.h>  /* Standard Input/Output  z.B. scanf, printf */
@@ -23,11 +23,9 @@ void sieb (int limit, int array[])
 {
 	int i, j;
 
-	for (i = 2; i <= sqrt(limit); i++) /* Bis zur Wurzel auf Primzal prüfen. */
-	   if (array[i])
-	   {
-	      for (j = 2 * i; j < limit; j += i) array[j] = NICHT_PRIM; /* Alle Vielfachen als Nicht-Primzahl makieren. */
-	   }
+	for (i = 2; i <= sqrt((float)limit); i++) /* Bis zur Wurzel auf Primzahl prÃ¼fen. */
+		if (array[i] == PRIM)
+			for (j = 2 * i; j < limit; j += i) array[j] = NICHT_PRIM; /* Alle Vielfachen als Nicht-Primzahl makieren. */
 }
 
 void ausgabe (int limit, int array[])
@@ -35,8 +33,6 @@ void ausgabe (int limit, int array[])
 	int i;
 
 	for (i = 0; i < limit; i++)
-	{
-		if (array[i] == 1)
+		if (array[i] == PRIM)
 			printf("%d, ", i);
-	}
 }
