0737e921f3aa9e8fb7390e0da965d5cb7919aec1
Felix <felix@stkn.org>
2012-12-04 15:17:42 +0100

    a2 implementation

--- u10/strings.c
+++ u10/strings.c
@@ -2,32 +2,39 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define ASCII 256
+
 void line (int n, char c);
 void printAsciiFreq (char *text[], const int n);
 
-void printText(char *text[], const int n);
-// gibt einen Text ((Feld von Zeichenketten) mit n Worten aus
-void textReverse(char *text[], const int n);
-// Invertiert in einem Text (Feld von Zeichenketten) die Reihenfolge
-// der Wörter; die Invertierung erfolgt ausschließlich durch Vertauschung
-// von Zeigern, nicht durch umkopieren der Wörter!
-void deleteText(char *text[], const int n);
-// gibt den Speicherplatz für die Wörter des Textes wieder frei
+void printText (char *text[], const int n);
+void textReverse (char *text[], const int n);
+void deleteText (char *text[], const int n);
 
 
 int main (int argc, char *argv[]) 
 {
-	char **feld;
-	feld = malloc(argc * sizeof(char *) + 1);
+	int i;
+	char **array;
+	// speicher allozierung: pointers auf strings
+	array = malloc((argc-1) * sizeof(char *));	// anzahl der argumente
+	if (array == NULL) exit(0);
 
-	feld[0] = argv[0];
-	feld[1] = argv[1];
-	feld[2] = argv[2];
-	feld[3] = argv[3];
+	// speicher allozierung: der einzelnen strings
+	for (i = 1; i < argc; i++)
+	{
+		// länge des arguments
+		size_t ln = strlen(argv[i]);
 
-	printf("%s\n", feld[3]);
+		// länge im array allozieren
+		array[i-1] = malloc((ln + 1) * sizeof(char));
+		if (array[i-1] == NULL) exit(0);
 
-	free(feld);
+		// argument ins array kopieren
+		strcpy(array[i-1], argv[i]);
+	}
+
+	pstrcpy(array, argv, argc);
 
 	line(23, '-');
 	printf("Statistik Kommandozeile\n");
@@ -37,9 +44,22 @@ int main (int argc, char *argv[])
 	printAsciiFreq(argv, argc);
 
 	printf("\n");
-	printf("Aufgabe: 2\n");
+	printf("\n");
 
-	printText(argv, argc);
+
+	line(23, '-');
+	printf("Aufgabe 2\n");
+	line(23, '-');
+
+	printText(array, argc - 1);
+
+	textReverse(array, argc -1);
+
+	printText(array, argc - 1);
+
+	deleteText(array, argc - 1);
+
+	free(array);
 
 	return 0;
 }
@@ -55,7 +75,7 @@ void line (int n, char c) 					// gibt n mal das Zeichen c aus
 void printAsciiFreq (char *text[], const int n)
 {
 	int i;
-	int chars[256] = { 0 };  // ascii tabell mit nullen vorinitialisieren
+	int chars[ASCII] = { 0 };  // ascii tabell mit nullen vorinitialisieren
 
 	// schleife: argumente
 	for (i = 1; i < n; i++)
@@ -67,7 +87,7 @@ void printAsciiFreq (char *text[], const int n)
 	}
 
 	// ausgabe der häufigkeit
-	for (i = 0; i < 256; i++)
+	for (i = 0; i < ASCII; i++)
 		if (chars[i] != 0)
 		{
 			printf("%c: ", i);
@@ -77,10 +97,30 @@ void printAsciiFreq (char *text[], const int n)
 	printf("\n");
 }
 
-void printText(char *text[], const int n)
+void printText (char *text[], const int n)
 {
 	int i;
-	for (i = 1; i < n; i++)
+	for (i = 0; i < n; i++)
 		printf("%s ", text[i]);
 	printf("\n");
+}
+
+void textReverse (char *text[], const int n)
+{
+	int i, j;
+	char *tmp;
+
+	for (i = 0, j = n - 1; i < n / 2; i++, j--)
+	{
+		tmp = text[i];
+		text[i] = text[j];
+		text[j] = tmp;
+	}
+}
+
+void deleteText (char *text[], const int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		free(text[i]);
 }
