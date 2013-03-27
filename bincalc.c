0e9da3f6b0f820029d42502d5bb9619389d36963
Felix <felix@stkn.org>
2013-03-27 08:34:30 +0100

    add bin calcualtor

---
+++ bincalc.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define LN 255
+
+typedef struct mystruct_s
+{
+	int32_t i;
+	float f;
+	char str[LN];
+} mystruct_t;
+
+void itobin (int32_t);
+void ftobin (float);
+void strtobin (char *);
+
+int main(int argc, char const *argv[])
+{
+	mystruct_t my;
+
+	// Integer
+	printf("Integer: ");
+	scanf("%d", &my.i);
+
+	itobin(my.i);
+
+	// Float
+	printf("Float: ");
+	scanf("%f", &my.f);
+
+	ftobin(my.f);
+
+	// String
+	printf("String: ");
+	scanf("%s", my.str);
+
+	strtobin(my.str);
+
+	return 0;
+}
+
+void itobin (int32_t integer)
+{
+	int i;
+
+	for (i = 31; i >= 0; i--)
+	{
+		printf("%c", ((integer & (1 << i)) != 0) + '0');
+	}
+
+	printf("\n\n");
+}
+
+void ftobin (float f)
+{
+	int i;
+	int32_t w;
+
+	w = *((int32_t *) (&f));
+
+	for (i = 31; i >= 0; i--)
+	{
+		switch (i)
+		{
+			case 31:
+				printf("Sign:\n");
+				break;
+			case 30:
+				printf("Exponent:\n");
+				break;
+			case 22:
+				printf("Significand:\n");
+		}
+
+		printf("%c", ((w & (1 << i)) != 0) + '0');
+
+		switch (i)
+		{
+			case 31:
+			case 23:
+			case 0:
+				printf("\n");
+		}
+	}
+
+	printf("\n");
+}
+
+void strtobin (char *str)
+{
+	int i, j;
+
+	i = 0;
+
+	while (str[i] != 0)
+	{
+		printf("%c - %d - 0x%x - ", str[i], (int)str[i], (int)str[i]);
+
+		// Bin Umrechnung
+		for (j = 7; j >= 0; j--)
+		{
+			printf("%c", ((str[i] & (1 << j)) != 0) + '0');
+		}
+
+		printf("\n");
+
+		i++;
+	}
+
+	printf("\n");
+}
